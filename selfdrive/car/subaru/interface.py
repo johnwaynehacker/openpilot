9b632730e52ec97075125914f97ba61d8ad2a9ba
johnwaynehacker <johnwaynehacker@gmail.com>
2018-10-04 19:24:10 -0400

    changes to see if steering actuated
    bugsy [10:24 AM]
    set Kf to 0.000002 and Kp to 0.03 and it should try to steer you off the road

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -66,9 +66,9 @@ class CarInterface(object):
       ret.steerRatio = 14
       ret.steerActuatorDelay = 0.3
       ret.steerRateCost = 0
-      ret.steerKf = 0.000005
+      ret.steerKf = 0.000002
       ret.steerKiBP, ret.steerKpBP = [[0.], [0.]] # m/s
-      ret.steerKpV, ret.steerKiV = [[0.0003], [0.00]]
+      ret.steerKpV, ret.steerKiV = [[0.03], [0.00]]
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
 
